08aa05a0536ed43bf31168be0d4644916772c4bc
Your Name <you@example.com>
2023-03-07 20:20:37 +0300

    update code

--- c++_code.cpp
+++ c++_code.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 
 int main() {
+   // Define variables for the two input numbers, and for the resulting sum, sub, mul, division, and average.
    float num1, num2, sum, sub, mul, division, average;
 
+   // Ask the user to enter two numbers, and read them from the console.
    cout << "Enter two numbers: ";
    cin >> num1 >> num2;
-//sum
+
+   // Calculate the sum, sub, mul, division, and average of the two numbers.
    sum = num1 + num2;
-//sub
    sub = num1 - num2;
-//mul
    mul = num1 * num2;
-//div
    division = num1 / num2;
    average = (num1 + num2) / 2;
 
+   // Print the results of the calculations to the console.
    cout << "Sum = " << sum << endl;
    cout << "Sub = " << sub << endl;
    cout << "mul = " << mul << endl;
    cout << "division = " << division << endl;
    cout << "Average = " << average << endl;
 
+   // Return 0 to indicate that the program executed successfully.
    return 0;
 }
